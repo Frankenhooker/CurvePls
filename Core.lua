00236bfd652a7f795a52bf21c43556ae8ad6324b
Frankenhooker <ichstinkedollwiekacke@googlemail.com>
2017-06-27 21:21:11 +0200

    Implement /fake command

--- Core.lua
+++ Core.lua
@@ -27,3 +27,21 @@ if msg == 'help' then
  end
 end
 SlashCmdList["KEYMASTERPLS"] = handler2;
+
+SLASH_ANYTHINGPLS1= '/fake';
+local function handler3(msg, editbox)
+local a, m, d, y = strsplit(",",msg)
+IDNumber, Name = GetAchievementInfo(a)
+  if Name == nil then print("Invalid Achivement ID!")
+  elseif msg == 'help' then
+  print("Enter /fake achievementID,month,day,year to fake an achievement, (For Example /fake 11162,6,6,17). \n If no dates are given, the current date will be taken.")
+  elseif string.len(msg) < 6 then
+  weekday, month, day, year = CalendarGetDate();
+  DEFAULT_CHAT_FRAME:AddMessage("\124cffffff00\124Hachievement:".. a ..":"..string.gsub(UnitGUID("player"), '0x', '')..":1:".. month ..":".. day ..":".. year-2000 ..":4294967295:4294967295:4294967295:4294967295\124h[".. Name .."]\124h\124r")
+  elseif string.len(msg) > 5 then if string.len(y) > 2 then y=y-2000 end
+  DEFAULT_CHAT_FRAME:AddMessage("\124cffffff00\124Hachievement:".. a ..":"..string.gsub(UnitGUID("player"), '0x', '')..":1:".. m ..":".. d ..":".. y ..":4294967295:4294967295:4294967295:4294967295\124h[".. Name .."]\124h\124r")
+  else
+  print("Bad Input!")
+ end
+end
+SlashCmdList["ANYTHINGPLS"] = handler3;
